fd8644ee60ff4fb0e8d773d7ea731113c940d2a6
uni51 <unieye51@gmail.com>
2022-11-19 14:13:12 +0900

    NextからLaravelにAPIリクエストを送信（Laravel側実装）

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('test', static function () {
+    return \App\Models\Sample::first();
+});
+
+Route::post('test', static function(Request $request) {
+    $request->validate(['text' => 'required|string']);
+    return \App\Models\Sample::create(['text' => $request->input('text')]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
