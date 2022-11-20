8ab0c5a162f6df44055d3b37fb72bb8c3e0c86ff
uni51 <unieye51@gmail.com>
2022-11-20 14:16:24 +0900

    メモデータ取得APIの実装

---
+++ app/Http/Controllers/MemoController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\MemoPostRequest;
+use App\Http\Resources\MemoResource;
+use App\Models\Memo;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Auth;
+
+class MemoController extends Controller
+{
+    /**
+     * メモの全件取得
+     * @return AnonymousResourceCollection
+     */
+    public function fetch(): AnonymousResourceCollection
+    {
+        // ログインユーザーのID取得
+        $id = Auth::id();
+        if (!$id) {
+            throw new Exception('未ログインです。');
+        }
+
+        try {
+            $memos = Memo::where('user_id', $id)->get();
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+        return MemoResource::collection($memos);
+    }
+
+    /**
+     * メモの登録
+     * @param MemoPostRequest $request
+     * @return JsonResponse
+     */
+    public function create(MemoPostRequest $request): JsonResponse
+    {
+        // 処理
+    }
+}
