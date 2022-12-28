de437bb0103f23ce3889b62c60b03d2cb94eb74e
uni51 <unieye51@gmail.com>
2022-12-28 10:09:02 +0900

    Exceptionの追加

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -43,8 +44,25 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (NotFoundHttpException $exception) {
+            return $this->apiError('Not Found', 404);
         });
+
+        $this->renderable(function (ApiException $exception) {
+            if ($exception->hasDetails()) {
+                return response()->json($exception->getDetails(), $exception->getCode());
+            }
+
+            return $this->apiError($exception->getMessage(), $exception->getCode());
+        });
+
+        $this->renderable(function (Throwable $exception) {
+            return $this->apiError('Internal Server Error', 500);
+        });
+    }
+
+    protected function apiError($message, $code)
+    {
+        return response()->json(['message' => $message], $code);
     }
 }
