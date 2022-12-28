08dd0bd84c56e34e1714bc3c66305fa98703a6f1
uni51 <unieye51@gmail.com>
2022-12-28 10:14:46 +0900

    Messageモデルとサービスの作成

---
+++ app/Models/Message.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models;
+
+class Message
+{
+    public $text;
+
+    public function __construct($text)
+    {
+        $this->text = $text;
+    }
+
+    public function toArray()
+    {
+        return [
+            'text' => $this->text
+        ];
+    }
+}
---
+++ app/Services/MessageServiceInterface.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Services;
+
+use App\Models\Message;
+
+interface MessageServiceInterface
+{
+    public function getPublicMessage(): Message;
+
+    public function getProtectedMessage(): Message;
+
+    public function getAdminMessage(): Message;
+}
