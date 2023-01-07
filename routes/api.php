fdfa46e4c5df55792c65a9fc1748fe564d243da9
uni51 <unieye51@gmail.com>
2023-01-08 07:34:42 +0900

    sail artisan breeze:install api の実行

--- routes/api.php
+++ routes/api.php
@@ -14,15 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('test', function () {
-    return \App\Models\Sample::first();
-});
-
-Route::post('test', function(Request $request) {
-    $request->validate(['text' => 'required|string']);
-    return \App\Models\Sample::create(['text' => $request->input('text')]);
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
