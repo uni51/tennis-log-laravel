48744d2431c70e8eb74dd665162ec8cd72fffc3f
uni51 <unieye51@gmail.com>
2023-01-08 13:35:30 +0900

    モデルにリレーション等の定義

--- app/Models/OAuthProvider.php
+++ app/Models/OAuthProvider.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OAuthProvider extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'provider',
+        'provider_id',
+        'user_id',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
