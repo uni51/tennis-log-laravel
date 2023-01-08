59788dc0ee6e6cf9f6a0db5f0ddd177df9a62df6
uni51 <unieye51@gmail.com>
2023-01-08 14:23:17 +0900

    GoogleとGitHUB認証の追加

--- app/Http/Controllers/OAuthProviderController.php
+++ app/Http/Controllers/OAuthProviderController.php
@@ -3,29 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Models\OAuthProvider;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
 
 class OAuthProviderController extends Controller
 {
+    const GITHUB = 'github';
+    const GOOGLE = 'google';
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(string $provider)
     {
-        //
+        return Socialite::driver($provider)->redirect();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+
+    public function store(string $provider)
     {
-        //
+        $socialite = Socialite::driver($provider)->stateless()->user();
+
+        $user = User::firstOrCreate([
+            'email' => $socialite->getEmail(),
+        ], [
+            'name' => $socialite->getName(),
+            //'name' => $socialite->getNickname(),
+        ]);
+
+        $user->providers()->updateOrCreate([
+            'provider' => self::GOOGLE,
+            'provider_id' => $socialite->getId(),
+        ]);
+
+        Auth::login($user);
+
+        return redirect(config('app.frontend_url'));
     }
 
     /**
--- routes/auth.php
+++ routes/auth.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\OAuthProviderController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
@@ -35,3 +36,11 @@ Route::post('/email/verification-notification', [EmailVerificationNotificationCo
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->middleware('auth')
                 ->name('logout');
+
+Route::middleware(['guest'])
+    ->prefix('login')
+    ->name('login')
+    ->group(function () {
+        Route::get('{provider}', [OAuthProviderController::class, 'index'])->name('provider');
+        Route::get('{provider}/callback', [OAuthProviderController::class, 'store'])->name('provider.callback');
+    });
