b7d1d15c4bb39ccd932287b62b1ccc2063f84727
uni51 <unieye51@gmail.com>
2023-01-08 13:25:34 +0900

    sail artisan make:model OAuthProvider -mcf --api

---
+++ app/Http/Controllers/OAuthProviderController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OAuthProvider;
+use Illuminate\Http\Request;
+
+class OAuthProviderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OAuthProvider  $oAuthProvider
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OAuthProvider $oAuthProvider)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OAuthProvider  $oAuthProvider
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OAuthProvider $oAuthProvider)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OAuthProvider  $oAuthProvider
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OAuthProvider $oAuthProvider)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_01_08_042450_create_o_auth_providers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('o_auth_providers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('o_auth_providers');
+    }
+};
