1b8077c60cf7406af7f48618330228ee388d694c
uni51 <unieye51@gmail.com>
2023-01-13 23:26:31 +0900

    memosテーブルのマイグレーションファイルと、シーダーの作成

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Seed the application's database.
@@ -14,9 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            UserSeeder::class,
-            MemoSeeder::class
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
