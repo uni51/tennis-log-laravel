27842a565e074188377a575cd786048986802e8e
uni51 <unieye51@gmail.com>
2023-01-28 12:16:35 +0900

    UsersTableSeederの作成と登録

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,6 +14,11 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
