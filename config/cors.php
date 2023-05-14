34e9a5fe3c5118005fb181ab6134273bcdd78fe8
uni51 <unieye51@gmail.com>
2023-05-14 13:23:59 +0900

    sanctum用 検証 01

--- config/cors.php
+++ config/cors.php
@@ -20,8 +20,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => ['*'],  // default
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => ['*'],  // default
+    // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
