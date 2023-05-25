2e3a5bacb0ab089d26d923249dcb3666290d1be4
uni51 <unieye51@gmail.com>
2023-05-25 09:14:02 +0900

    ログイン用のコントローラーをbreeze生成時のものを利用するように修正

--- app/Http/Controllers/UserAuth/AuthenticatedSessionController.php
+++ app/Http/Controllers/UserAuth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\UserAuth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserAuth\LoginRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -13,13 +14,14 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): UserResource
     {
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return response()->noContent();
+        //return response()->noContent();
+        return new UserResource(Auth::user());
     }
 
     /**
