e211a77d27c0d00cd4228566bff6566818fdd41a
uni51 <unieye51@gmail.com>
2023-07-30 16:25:21 +0900

    axiosのinterceptorsを使ったFirebaseのidToken送信にも対応

--- app/Http/Controllers/UserAuth/FirebaseAuthController.php
+++ app/Http/Controllers/UserAuth/FirebaseAuthController.php
@@ -34,9 +34,13 @@ class  FirebaseAuthController extends Controller
     public function login(Request $request): JsonResponse
     {
         $id_token = $request->input('idToken');
-        
+//        $id_token = $request->headers->get('authorization');
+
+        Log::debug($id_token);
+
         try {
             $verifiedIdToken = $this->auth->verifyIdToken(json_decode($id_token));
+//            $verifiedIdToken = $this->auth->verifyIdToken($id_token);
         } catch (FailedToVerifyToken $e) {
             Log::debug($e->getMessage());
             return response()->json([
