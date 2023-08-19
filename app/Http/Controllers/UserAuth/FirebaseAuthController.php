d18865b91ed50039ce2f744f1c4643fa7b2f9ecd
uni51 <unieye51@gmail.com>
2023-08-19 15:39:07 +0900

    ログイン・ログアウトメソッドでのトランザクション対応

--- app/Http/Controllers/UserAuth/FirebaseAuthController.php
+++ app/Http/Controllers/UserAuth/FirebaseAuthController.php
@@ -41,62 +41,73 @@ class  FirebaseAuthController extends Controller
 
         try {
             $verifiedIdToken = $this->auth->verifyIdToken($id_token);
+
+            $firebaseUid = $verifiedIdToken->claims()->get('sub');
+            $firebaseUser = $this->auth->getUser($firebaseUid);
+
+            $user = User::select('users.*')
+                ->where('firebase_logins.firebase_uid', '=', $firebaseUid)
+                ->leftJoin('firebase_logins', 'users.id', '=', 'firebase_logins.user_id')
+                ->first();
+
+            // トランザクション開始
+            DB::beginTransaction();
+
+            if (is_null($user)) {
+                $checkExistUser = User::where('email', $firebaseUser->email)->first();
+
+                if (is_null($checkExistUser)) {
+                    $user = User::create([
+                        'name' => $firebaseUser->displayName,
+                        'nickname' => $firebaseUser->displayName,
+                        'email' => $firebaseUser->email,
+                    ]);
+                } else {
+                    $user = $checkExistUser;
+                }
+                // Log::debug('Login User:' . $user);
+            }
+
+            $tokenResult = $user->createToken('Personal Access Token');
+
+            // Log::debug('Login Token ID:' . $tokenResult->token->id);
+            // Log::debug('Login accessToken:' . $tokenResult->accessToken);
+
+            $expires_at = Carbon::now()->addWeeks(1);
+
+            $firebaseLoginUser = FirebaseLogin::create([
+                'user_id' => $user->id,
+                'firebase_uid' => $firebaseUid,
+                'token_id' => $tokenResult->token->id,
+                'access_token' => $tokenResult->accessToken,
+                'expires_at' => Carbon::parse(
+                    $expires_at
+                )->toDateTimeString()
+            ]);
+
+            // Log::debug('FirebaseLoginUser:' . $firebaseLoginUser);
+
+            // コミット
+            DB::commit();
+
+            return response()->json([
+                'uid' => $firebaseUid,
+                // ネームが設定されないことは基本的にはない筈だが、念の為の処理
+                'name' => $firebaseUser->displayName ?? $firebaseUser->email,
+                'token' => $tokenResult->accessToken ?? $user->access_token,
+            ]);
+
         } catch (FailedToVerifyToken $e) {
             Log::debug($e->getMessage());
             return response()->json([
-                'error' => $e->getMessage(),
+                'error' => 'FailedToVerifyToken' . $e->getMessage(),
+            ]);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json([
+                'error' => 'LoginError' . $e->getMessage(),
             ]);
         }
-
-        $firebaseUid = $verifiedIdToken->claims()->get('sub');
-        $firebaseUser = $this->auth->getUser($firebaseUid);
-
-        $user = User::select('users.*')
-            ->where('firebase_logins.firebase_uid', '=', $firebaseUid)
-            ->leftJoin('firebase_logins', 'users.id', '=', 'firebase_logins.user_id')
-            ->first();
-
-        if (is_null($user)) {
-            $checkExistUser = User::where('email', $firebaseUser->email)->first();
-
-            if (is_null($checkExistUser)) {
-                $user = User::create([
-                    'name' => $firebaseUser->displayName,
-                    'nickname' => $firebaseUser->displayName,
-                    'email' => $firebaseUser->email,
-                ]);
-            } else {
-                $user = $checkExistUser;
-            }
-
-            // Log::debug('Login User:' . $user);
-        }
-
-        $tokenResult = $user->createToken('Personal Access Token');
-
-        // Log::debug('Login Token ID:' . $tokenResult->token->id);
-        // Log::debug('Login accessToken:' . $tokenResult->accessToken);
-
-        $expires_at = Carbon::now()->addWeeks(1);
-
-        $firebaseLoginUser = FirebaseLogin::create([
-            'user_id' => $user->id,
-            'firebase_uid' => $firebaseUid,
-            'token_id' => $tokenResult->token->id,
-            'access_token' => $tokenResult->accessToken,
-            'expires_at' => Carbon::parse(
-                $expires_at
-            )->toDateTimeString()
-        ]);
-
-        // Log::debug('FirebaseLoginUser:' . $firebaseLoginUser);
-
-        return response()->json([
-            'uid' => $firebaseUid,
-            // ネームが設定されないことは基本的にはない筈だが、念の為の処理
-            'name' => $firebaseUser->displayName ?? $firebaseUser->email,
-            'token' => $tokenResult->accessToken ?? $user->access_token,
-        ]);
     }
 
     /**
@@ -107,18 +118,22 @@ class  FirebaseAuthController extends Controller
         $id_token = $request->headers->get('authorization');
         $token = trim(str_replace('Bearer', '', $id_token));
 
-        $firebaseLoginUser = FirebaseLogin::where('access_token', $token)->first();
-
-        $oauthAccessTokens = OAuthAccessTokens::where('id', $firebaseLoginUser->token_id)
-            ->where('user_id', $firebaseLoginUser->user_id)
-            ->first();
-
-        DB::beginTransaction();
         try {
+            // トランザクション開始
+            DB::beginTransaction();
+
+            $firebaseLoginUser = FirebaseLogin::where('access_token', $token)->first();
+            $oauthAccessTokens = OAuthAccessTokens::where('id', $firebaseLoginUser->token_id)
+                ->where('user_id', $firebaseLoginUser->user_id)
+                ->first();
+
             // 以下の書き方だと、oauth_access_tokensテーブル内の、同一ユーザーIDに紐づくデータが全部消えてしまうので、個別にデータを削除する
             //auth('front_api')->user()->tokens()->delete();
             $firebaseLoginUser->delete();
             $oauthAccessTokens->delete();
+
+            // コミット
+            DB::commit();
         } catch (\Exception $e) {
             DB::rollback();
         }
