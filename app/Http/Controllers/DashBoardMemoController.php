420ec9ab82016b890f6b1ef8a461b17571935ce9
uni51 <unieye51@gmail.com>
2024-01-16 06:33:51 +0900

    DashBoardMemoControllerにPHPDoc追加

--- app/Http/Controllers/DashBoardMemoController.php
+++ app/Http/Controllers/DashBoardMemoController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Dashboard\DashboardMemosCategoryRequest;
+use App\Http\Requests\Dashboard\DashboardMemosCategoryTagRequest;
 use App\Http\Requests\Dashboard\DashboardMemosStatusRequest;
+use App\Http\Requests\Dashboard\DashboardMemosTagRequest;
 use App\Http\Requests\DashboardMemoSearchRequest;
 use App\Http\Requests\MemoEditRequest;
 use App\Http\Requests\MemoPostRequest;
@@ -28,44 +30,92 @@ class DashBoardMemoController extends Controller
     /**
      * メモの公開・非公開を問わずに、そのユーザーに紐づく記事一覧を取得するAPI
      *
+     * @param MemoService $service
      * @return AnonymousResourceCollection
      * @throws Exception
      */
-    public function list(MemoService $service)
+    public function list(MemoService $service): AnonymousResourceCollection
     {
         return $service->listMemoLinkedToUser(Auth::id());
     }
 
-    public function search(DashboardMemoSearchRequest $request, MemoService $service)
+    /**
+     * キーワードによる記事検索API
+     *
+     * @param DashboardMemoSearchRequest $request
+     * @param MemoService $service
+     * @return AnonymousResourceCollection
+     */
+    public function search(DashboardMemoSearchRequest $request, MemoService $service): AnonymousResourceCollection
     {
-        $memos = $service->dashboardMemoSearch(Auth::id(), $request);
+        $memos = $service->dashboardMemoSearch(Auth::id(), $request->q);
 
         return MemoResource::collection($memos);
     }
 
-    public function memoListByStatus(DashboardMemosStatusRequest $request, MemoService $service)
+    /**
+     * @param DashboardMemosStatusRequest $request
+     * @param MemoService $service
+     * @return AnonymousResourceCollection
+     * @throws Exception
+     */
+    public function memoListByStatus(
+        DashboardMemosStatusRequest $request,
+        MemoService $service
+    ): AnonymousResourceCollection
     {
         return $service->memoListByStatus(Auth::id(), $request->status);
     }
 
-    public function memoListByCategory(DashboardMemosCategoryRequest $request, MemoService $service)
+    /**
+     * カテゴリー別 記事一覧取得API
+     *
+     * @param DashboardMemosCategoryRequest $request
+     * @param MemoService $service
+     * @return AnonymousResourceCollection
+     * @throws Exception
+     */
+    public function memoListByCategory(
+        DashboardMemosCategoryRequest $request,
+        MemoService $service
+    ): AnonymousResourceCollection
     {
         return $service->memoListByCategory(Auth::id(), $request->categoryId);
     }
 
-    public function memoListByTag(MemoService $service, $tag)
+    /**
+     * タグ別 記事一覧取得API
+     *
+     * @param DashboardMemosTagRequest $request
+     * @param MemoService $service
+     * @return AnonymousResourceCollection
+     * @throws Exception
+     */
+    public function memoListByTag(DashboardMemosTagRequest $request, MemoService $service): AnonymousResourceCollection
     {
-        return $service->memoListByTag(Auth::id(), $tag);
-    }
-    public function memoListByCategoryAndTag(MemoService $service, $categoryId, $tag)
-    {
-        return $service->memoListByCategoryAndTag(Auth::id(), $categoryId, $tag);
+        return $service->memoListByTag(Auth::id(), $request->tag);
     }
 
     /**
-     * メモの登録
+     * カテゴリーおよびタグによる記事一覧取得API
+     *
+     * @param DashboardMemosCategoryTagRequest $request
+     * @param MemoService $service
+     * @return AnonymousResourceCollection
+     * @throws Exception
+     */
+    public function memoListByCategoryAndTag(
+        DashboardMemosCategoryTagRequest $request,
+        MemoService $service
+    ): AnonymousResourceCollection
+    {
+        return $service->memoListByCategoryAndTag(Auth::id(), $request->categoryId, $request->tag);
+    }
+
+    /**
      * @param MemoPostRequest $request
      * @return JsonResponse
+     * @throws Exception
      */
     public function create(MemoPostRequest $request): JsonResponse
     {
