78d1583a654bd863a8f48ecf0148e8532eade16f
uni51 <unieye51@gmail.com>
2024-02-24 10:44:00 +0900

    マイグレーションファイルの修正等

---
+++ database/migrations/2024_02_24_093739_create_deleted_tags_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deleted_tags', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('force_deleted');
+            $table->unsignedBigInteger('tag_id')->comment('タグID');
+            $table->string('name');
+            $table->string('normalized');
+            $table->foreignId('created_by')->nullable();
+            $table->boolean('created_by_admin')->comment('管理者が作成したタグかどうか');
+            $table->boolean('is_appropriate')->comment('内容が適切か');
+            $table->foreignId('reviewed_by')->nullable()->comment('誰に審査されたか');
+            $table->dateTime('tag_created_at')->comment('タグ作成日時');
+            $table->dateTime('tag_updated_at')->comment('タグ更新日時');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deleted_tags');
+    }
+};
---
+++ database/migrations/2024_02_24_095117_create_deleted_memo_tag_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+class CreateMemoTagTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deleted_memo_tag', function (Blueprint $table) {
+            $table->foreignId('memo_id');
+            $table->foreignId('tag_id');
+            $table->dateTime('memo_tag_created_at')->comment('作成日時');
+            $table->dateTime('memo_tag_updated_at')->comment('更新日時');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deleted_memo_tag');
+    }
+}
