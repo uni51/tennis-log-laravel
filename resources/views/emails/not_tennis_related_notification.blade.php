100f5bef567a668b64629e8b6502b29dfbc13a20
uni51 <unieye51@gmail.com>
2024-03-24 14:00:33 +0900

    テニスに関連しないメモが投稿された場合の、メールにメモの公開ステータスを記載するよう変更

--- resources/views/emails/not_tennis_related_notification.blade.php
+++ resources/views/emails/not_tennis_related_notification.blade.php
@@ -10,10 +10,22 @@
 @if($memo)
     <p>{{ "メモの種別: 更新" }}</p>
     <p>メモのid: {{ $memo->id }}</p>
+    <p>メモのステータス:
+        @if($memo->status === 0){{ "下書き" }} @endif
+        @if($memo->status === 1){{ "公開" }} @endif
+        @if($memo->status === 2){{ "シェア" }} @endif
+        @if($memo->status === 3){{ "非公開" }} @endif
+    </p>
     <p>{{ "投稿者のid: " . $user->id }}</p>
     <p>{{ "投稿者のnickname: " . $user->nickname }}</p>
 @else
     <p>{{ "メモの種別: 新規作成" }}</p>
+    <p>メモのステータス:
+        @if($memo->status === 0){{ "下書き" }} @endif
+        @if($memo->status === 1){{ "公開" }} @endif
+        @if($memo->status === 2){{ "シェア" }} @endif
+        @if($memo->status === 3){{ "非公開" }} @endif
+    </p>
     <p>{{ "投稿者のid: " . $user->id }}</p>
     <p>{{ "投稿者のnickname: " . $user->nickname }}</p>
 @endif
